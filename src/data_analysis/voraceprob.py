582c7eb6f0af2aa4f4292f60e1cdfabe18ef5c8d
AdrienDoum <adrien.doumergue@hotmail.fr>
2016-11-04 09:02:25 -0400

    Update voraceprob.py

--- src/data_analysis/voraceprob.py
+++ src/data_analysis/voraceprob.py
@@ -1,15 +1,16 @@
 import random
 random.seed()
-tower=[]
-hauteurs=[j[2] for j in blocks[0]]
-hauteur=0
-tower.append(Block([1000,1000,0]))
-for j in range(len(blocks[0])):
-    if blocks[0][j].posersur(tower[-1]) :
-        haut=blocks[0][j][2]
-        test=float((haut-min(hauteurs))/(max(hauteurs)-min(hauteurs)))
-        if (random.random()<test):
-            tower.append(blocks[0][j])
-            hauteur+=blocks[0][j][2]
+def vorace(blocks):
+    tower=[]
+    hauteurs=[j[0] for j in blocks]
+    hauteur=0
+    tower.append(Block([1000,1000,0]))
+    for j in blocks :
+        if j.posersur(tower[-1]) :
+            haut=j[0]
+            test=float((haut-min(hauteurs))/(max(hauteurs)-min(hauteurs)))
+            if (random.random()<test):
+                tower.append(j)
+                hauteur+=j[0]
 
-print(hauteur,len(tower))
+    return hauteur,len(tower)
